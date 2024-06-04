431385383b92834caf3de86b525519d2ac142b6b
arindam1997007 <arindamchy00@gmail.com>
2024-06-05 04:35:50 +0530

    Firebase cloud function
    - Created onCreateUser function to run when new user is created, and create a document with userId in firestore DB

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -7,6 +7,14 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+// import { User } from "firebase/auth"
+import { auth } from "firebase-functions"
+import { UserRecord } from "firebase-admin/auth"
+import { getFirestore } from "firebase-admin/firestore"
+const { initializeApp } = require("firebase-admin/app")
+
+initializeApp()
+
 // import {onRequest} from "firebase-functions/v2/https";
 // import * as logger from "firebase-functions/logger";
 
@@ -17,3 +25,20 @@
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+export const onCreateUser = auth.user().onCreate(async (user: UserRecord) => {
+	console.log(`User Created with ${user.uid}`)
+	const uid = user.uid
+	const db = getFirestore()
+
+	const userRes = await db.collection("users").add({
+		uid,
+	})
+
+	await db.collection("users").doc(userRes.id).collection("tasks").add({
+		taskName: "",
+		taskDescription: "",
+		taskStatus: "",
+		dueDate: "",
+	})
+})
